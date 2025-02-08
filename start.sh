318674e23bc8ed9647ebc76da473d6b19cda2706
cubysoftmc <mc@cubysoft.uk>
2025-02-08 08:07:14 +0000

    cambios por obito

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 1. Instalar Playit y tmux
+sudo apt update && sudo apt install -y tmux
 echo "Instalando Playit y tmux..."
 curl -SsL https://playit-cloud.github.io/ppa/key.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/playit.gpg >/dev/null
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data ./" | sudo tee /etc/apt/sources.list.d/playit-cloud.list
